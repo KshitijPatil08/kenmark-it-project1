b9e98ab870b5c4e60325f49078adf6fc41ec99b0
kshitij patil <kshitijpatil0809@gmail.com>
2025-12-19 21:46:40 +0530

    Fix the issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-    const url = process.env.DATABASE_URL;
-    if (!url) {
-        throw new Error('DATABASE_URL is not defined');
-    }
-
     return new PrismaClient({
-        datasourceUrl: url,
+        datasourceUrl: process.env.DATABASE_URL as string,
         log: ['query', 'error', 'warn'],
     } as any)
 }
